72b6c948b65da54bf8fd65c6fd76bf66bdb2bf6e
AmarZarif <42981882+AmarZarif@users.noreply.github.com>
2018-11-25 19:53:13 -0500

    Update ChatMessage.java

--- ChatMessage.java
+++ ChatMessage.java
@@ -6,4 +6,29 @@ final class ChatMessage implements Serializable {
 
     // Here is where you should implement the chat message object.
     // Variables, Constructors, Methods, etc.
+
+    private int type;
+    private String message;
+    private String logoutMessage;
+
+    public ChatMessage(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+
+    public int getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    private void broadcast(String message){
+
+
+    }
+
+
 }
