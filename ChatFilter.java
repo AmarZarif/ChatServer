ffc0c22e18d0c144b92d70d863bd9ab8bfabac0b
raziqraif <42981908+raziqraif@users.noreply.github.com>
2018-11-26 13:38:38 -0500

    Update ChatFilter.java

--- ChatFilter.java
+++ ChatFilter.java
@@ -1,5 +1,11 @@
 
-
+/**
+ * ChatFilter - Project 4
+ *
+ * @author Muhammad Raziq Raif Ramli, mramli@purdue.edu
+ * @author Amar Zarif Azamin, aazamin@purdue.edu
+ * @version 11/26/2018
+ */
 public class ChatFilter {
 
     public ChatFilter(String badWordsFileName) {
